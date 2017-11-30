ff1bfdcdb2743cf3c38cd2e7e6c9f423e55e2ac7
Steger Patrick <pse@novatec-gmbh.de>
2017-11-30 18:45:22 +0100

    delete .idea and target

--- src/main/java/de/novatecgmbh/camunda/bpm/prototype/camunda/ServerRepo.java
+++
@@ -1,9 +0,0 @@
-package de.novatecgmbh.camunda.bpm.prototype.camunda;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ServerRepo extends CrudRepository<Server, String>{
-    public Server findByName(String name);
-}
