7f4504d0fb074cdc99c1edd478f1edceca70288f
stegerpa@gmail.com <5233stega>
2017-11-30 18:47:59 +0100

    Fixed API

--- src/main/java/de/novatecgmbh/camunda/bpm/prototype/camunda/DataController.java
+++ src/main/java/de/novatecgmbh/camunda/bpm/prototype/camunda/DataController.java
@@ -1,44 +1,73 @@
 package de.novatecgmbh.camunda.bpm.prototype.camunda;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class DataController {
-    @Autowired
-    private ServerRepo repository;
 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
+    public KeyValue returnHello() {
+        KeyValue hello = new KeyValue("name", "hello");
 
-    @RequestMapping("/servers/{name}")
-    public Server getServer(@PathVariable("name") String name){
-        return repository.findByName(name);
+        //Convert Java Object to JSON
+        //ObjectMapper mapper = new ObjectMapper();
+        return hello;
     }
 
-    //Returns all servers as List
-    @RequestMapping("/servers")
-    public Iterable<Server> getServers(){
-        return repository.findAll();
-    }
-
-
-    @PostMapping("/servers")
-    public ResponseEntity<String> addEntity(@RequestBody Server server){
-        repository.save(server);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-
-    @DeleteMapping("/servers/{name}")
-    public ResponseEntity<String> deleteEntity(@PathVariable String name){
-        Server s = repository.findByName(name);
-        if(s.getName().equals(name)) {
-            repository.delete(s);
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    //Return the address of a server stored in properties file
+    @RequestMapping("/server/{server}")
+    @JsonValue
+    public String getServerAddress(@PathVariable("server") String server) {
+        try {
+            PropertiesConfiguration conf = new PropertiesConfiguration("server.properties");
+            return (String) conf.getProperty(server);
+        } catch (ConfigurationException e) {
+            System.out.println(e.getMessage());
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return null;
+    }
+
+//    @RequestMapping("/servers/{name}")
+//    public Server getServer(@PathVariable("name") String name){
+//        return repository.findByName(name);
+//    }
+//
+//    //Returns all servers as List
+//    @RequestMapping("/servers")
+//    public Iterable<Server> getServers(){
+//        return repository.findAll();
+//    }
+//
+//
+//    @PostMapping("/servers")
+//    public ResponseEntity<String> addEntity(@RequestBody Server server){
+//        repository.save(server);
+//        return new ResponseEntity<>(HttpStatus.CREATED);
+//    }
+//
+//
+//    @DeleteMapping("/servers/{name}")
+//    public ResponseEntity<String> deleteEntity(@PathVariable String name){
+//        Server s = repository.findByName(name);
+//        if(s.getName().equals(name)) {
+//            repository.delete(s);
+//            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+//        }
+//        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//    }
+
+    class KeyValue {
+        private String key;
+        private String value;
+
+        KeyValue(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
 }
--- src/main/java/de/novatecgmbh/camunda/bpm/prototype/camunda/ProcessApplication.java
+++ src/main/java/de/novatecgmbh/camunda/bpm/prototype/camunda/ProcessApplication.java
@@ -4,6 +4,11 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication(scanBasePackages = "de.novatecgmbh.camunda.bpm.prototype")
 @EnableProcessApplication
